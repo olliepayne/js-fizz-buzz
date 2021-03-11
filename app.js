78779b9724c8ca0a45eabfc8efc969726c456ccf
olliepayne <olliepayne@me.com>
2021-03-11 14:47:05 -0700

    stub ideas

---
+++ app.js
@@ -0,0 +1,13 @@
+// Write a program that outputs the string representation of numbers from 1 to n.
+// But for multiples of three it should output “Fizz” instead of the number
+// for the multiples of five output “Buzz”. 
+// For numbers which are multiples of both three and five output “FizzBuzz”.
+
+// allow a user defined range
+// print out into a 'virtual terminal'
+
+function checkFizzBuzz(i = 0) {
+  console.log(i)
+}
+
+checkFizzBuzz()
