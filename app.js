283318f9005745b5e0e6fac76f69158058ecc665
olliepayne <olliepayne@me.com>
2021-03-13 12:19:34 -0700

    render output

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-// Write a program that outputs the string representation of numbers from 1 to n.
-// But for multiples of three it should output “Fizz” instead of the number
-// for the multiples of five output “Buzz”. 
-// For numbers which are multiples of both three and five output “FizzBuzz”.
-
 // allow a user defined range
 // print out into a 'virtual terminal'
+
+const outputEl = document.getElementById('output')
 
 function checkFizzBuzz(i, n) {
   if (i <= n) {
@@ -21,6 +18,7 @@ function checkFizzBuzz(i, n) {
     }
 
     console.log(output)
+    outputEl.innerText = outputEl.innerText + '\n' + output
 
     checkFizzBuzz(i + 1, n)
   }
